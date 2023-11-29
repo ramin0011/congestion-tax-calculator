a122897da45ae663802da1551dac04d97a00a665
Ramin <Me@RAMIN-LAPTOP>
2023-11-30 02:30:29 +0330

    add validation to create congestion time

--- congestion-tax-calculator-net-core/Services/CongestionTimeService.cs
+++ congestion-tax-calculator-net-core/Services/CongestionTimeService.cs
@@ -7,27 +7,33 @@ using congestion_tax_calculator_net_core_data.Repositories.Interfaces;
 
 namespace congestion.calculator.Services
 {
-    public  class CongestionTimeService :  ICongestionTimeService
+    public class CongestionTimeService : ICongestionTimeService
     {
         private ICongestionTimeRepository _congestionTimeRepository;
+
         public CongestionTimeService(ICongestionTimeRepository congestionTimeRepository)
         {
-           this._congestionTimeRepository=congestionTimeRepository;
+            this._congestionTimeRepository = congestionTimeRepository;
         }
+
         public Task<List<CongestionTime>> GetAll(string city)
         {
             return _congestionTimeRepository.GetAllByCity(city);
         }
+
         public async Task Create(string city, TimeSpan startTime, TimeSpan endTime, int fee)
         {
             city = city.ToLower();
-            await _congestionTimeRepository.Add(new CongestionTime()
-            {
-                EndTime = endTime,
-                City = city,
-                StartTime = startTime,
-                Fee = fee
-            });
+            if (await _congestionTimeRepository.Count(c => c.City == city &&
+                                                           c.StartTime == startTime &&
+                                                           c.EndTime == endTime) == 0)
+                await _congestionTimeRepository.Add(new CongestionTime()
+                {
+                    EndTime = endTime,
+                    City = city,
+                    StartTime = startTime,
+                    Fee = fee
+                });
         }
     }
 }
